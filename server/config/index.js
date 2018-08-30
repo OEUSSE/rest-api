6b506baf2c98870b305d06a0816ce8b38229b503
Óscar Eusse Flórez <scr.eusse@gmail.com>
2018-08-29 21:40:50 -0500

    [Modified] MONGO URI

--- server/config/index.js
+++ server/config/index.js
@@ -15,6 +15,6 @@ let urlDB =
   process.env.NODE_ENV === 'dev' ?
     'mongodb://localhost:27017/cafe_store'
     :
-    'mongodb://cafe-user:123456abc@ds035750.mlab.com:35750/cafe_store'
+    process.env.MONGO_URI
 
 process.env.URLDB = urlDB
